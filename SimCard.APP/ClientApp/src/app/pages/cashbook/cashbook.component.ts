24be83bc18034228f4f67020da8a3c2c3085870a
SangTran <tpgiangsang1906@gmail.com>
2019-01-18 23:48:21 +0700

    layout so tien mat component

--- SimCard.APP/ClientApp/src/app/pages/cashbook/cashbook.component.ts
+++ SimCard.APP/ClientApp/src/app/pages/cashbook/cashbook.component.ts
@@ -6,25 +6,43 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./cashbook.component.css"]
 })
 export class CashbookComponent implements OnInit {
-	isShowDialogPhieuChi: boolean = false;
-	isShowDialogPhieuThu: boolean = false;
-	constructor() { }
+  selectedDonVi: String = "";
+  LoaiDonVi = [
+    { label: "Shop 1", value: "CN1" },
+    { label: "Shop 2", value: "CN2" },
+    { label: "Shop 3", value: "CN3" },
+    { label: "Shop 4", value: "CN4" }
+  ];
+  selectedLoaiDuLieu: String = "";
+  LoaiDuLieu = [
+    { label: "PDF", value: "PDF" },
+    { label: "Image", value: "IMG" }
+  ];
+  tuNgay: any;
+  toiNgay: any;
+  tonDauKi: any;
+  tonCuoiKi: any;
+  soTienThu: any;
+  soTienChi: any;
+  isShowDialogPhieuChi: boolean = false;
+  isShowDialogPhieuThu: boolean = false;
+  constructor() {}
 
-	ngOnInit() { }
+  ngOnInit() {}
 
-	onShowDialogPhieuChi() {
-		this.isShowDialogPhieuChi = true;
-	}
+  onShowDialogPhieuChi() {
+    this.isShowDialogPhieuChi = true;
+  }
 
-	onGetIsShowDialogPhieuChi(data: any) {
-		this.isShowDialogPhieuChi = data;
-	}
+  onGetIsShowDialogPhieuChi(data: any) {
+    this.isShowDialogPhieuChi = data;
+  }
 
-	onShowDialogPhieuThu() {
-		this.isShowDialogPhieuThu = true;
-	}
+  onShowDialogPhieuThu() {
+    this.isShowDialogPhieuThu = true;
+  }
 
-	onGetIsShowDialogPhieuThu(data: any) {
-		this.isShowDialogPhieuThu = data;
-	}
+  onGetIsShowDialogPhieuThu(data: any) {
+    this.isShowDialogPhieuThu = data;
+  }
 }
--- SimCard.APP/ClientApp/src/app/public/home/home.component.ts
+++ SimCard.APP/ClientApp/src/app/public/home/home.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  selectedDonVi: String = "";
+  LoaiDonVi = [
+    { label: "Shop 1", value: "CN1" },
+    { label: "Shop 2", value: "CN2" },
+    { label: "Shop 3", value: "CN3" },
+    { label: "Shop 4", value: "CN4" }
+  ];
   constructor() { }
 
   ngOnInit() {
