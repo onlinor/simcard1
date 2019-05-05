0044084f1bd826bf988376cdc0f071eff9561c04
khuong <ptkhuong96@gmail.com>
2019-05-05 22:55:08 +0700

    add productexchange service, please get back connection.

--- SimCard.APP/Startup.cs
+++ SimCard.APP/Startup.cs
@@ -66,7 +66,7 @@ namespace SimCard.APP
             });
 
             services.AddAutoMapper();
-            services.AddDbContext<SimCardDBContext>(options => options.UseSqlServer("Server=.;Database=SimCard;Integrated Security=True;Trusted_Connection=True;"));
+            services.AddDbContext<SimCardDBContext>(options => options.UseSqlServer("Server=HC-UT40647N\\SQLEXPRESS;Database=SimcardDB;Trusted_Connection=True;MultipleActiveResultSets=true"));
             services.AddDefaultIdentity<User>();
             services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
             services.AddScoped<IProductRepository, ProductRepository>();
@@ -88,6 +88,7 @@ namespace SimCard.APP
             services.AddScoped<IReportService, ReportService>();
             services.AddScoped<IAuthService, AuthService>();
             services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<IProductExchangeService, ProductExchangeService>();
             services.AddCors();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
